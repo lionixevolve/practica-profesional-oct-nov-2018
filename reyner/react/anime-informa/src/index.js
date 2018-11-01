5a59ec535afe4444888ae1deddc3274e3e0d5018
Rey <Rey86581@gmail.com>
2018-11-01 16:55:24 -0600

    Change rutes

--- reyner/react/anime-informa/src/index.js
+++ reyner/react/anime-informa/src/index.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Contenedor } from './js-components/Contenedor';
+import { Contenedor } from './js-components-anime/Contenedor';
 
 class AnimeContainer extends Component {
   render() {
     return <Contenedor />
   }
+}
+
+class NarutoContenedor extends Component {
+    render() {
+      return <
+    }
 }
 
 ReactDOM.render(<AnimeContainer />, document.getElementById('app'));
