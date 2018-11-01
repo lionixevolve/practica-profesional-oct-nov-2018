0e565bd3c02908df86ed419fc9d67f5cdd9446f1
Rey <Rey86581@gmail.com>
2018-11-01 14:48:48 -0600

    Begin react app

---
+++ reyner/react/anime-informa/src/index.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class App extends Component {
+  render() {
+    return <h1>Hola mundo</h1>
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
