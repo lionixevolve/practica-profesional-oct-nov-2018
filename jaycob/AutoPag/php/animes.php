4803129c42a101f4bc025519696df5f2b82569fd
JaycobNy <jaycob.b.a@gmail.com>
2018-11-06 17:00:04 -0600

    rebuild

---
+++ jaycob/AutoPag/php/animes.php
@@ -0,0 +1,87 @@
+<?php
+  $myObj->name = "Fate/Stay";
+  $myObj->Genero = "Accion";
+  $myObj->Detalles = "...";
+
+  $myJSON = json_encode($myObj);
+
+  echo $myJSON;
+?>
+
+<?php
+  $myObj->name = "Aldonah";
+  $myObj->Genero = "Accion";
+  $myObj->Detalles = "...";
+
+  $myJSON = json_encode($myObj);
+
+  echo $myJSON;
+?>
+<?php
+$myObj->name = "Re:Creators";
+$myObj->Genero = "Accion";
+$myObj->Detalles = "...";
+
+$myJSON = json_encode($myObj);
+
+echo $myJSON;
+?>
+
+<?php
+$myObj->name = "Fate/Stay";
+$myObj->Genero = "Accion";
+$myObj->Detalles = "New York";
+
+$myJSON = json_encode($myObj);
+
+echo $myJSON;
+?>
+
+<?php
+$myObj->name = "Fate/Stay";
+$myObj->Genero = "Accion";
+$myObj->Detalles = "New York";
+
+$myJSON = json_encode($myObj);
+
+echo $myJSON;
+?>
+
+<?php
+$myObj->name = "Fate/Stay";
+$myObj->Genero = "Accion";
+$myObj->Detalles = "New York";
+
+$myJSON = json_encode($myObj);
+
+echo $myJSON;
+?>
+
+<?php
+$myObj->name = "Fate/Stay";
+$myObj->Genero = "Accion";
+$myObj->Detalles = "New York";
+
+$myJSON = json_encode($myObj);
+
+echo $myJSON;
+?>
+<?php
+$myObj->name = "Fate/Stay";
+$myObj->Genero = "Accion";
+$myObj->Detalles = "New York";
+
+$myJSON = json_encode($myObj);
+
+echo $myJSON;
+?>
+
+<?php
+$myObj->name = "Fate/Stay";
+$myObj->Genero = "Accion";
+$myObj->Detalles = "New York";
+
+$myJSON = json_encode($myObj);
+
+echo $myJSON;
+?>
