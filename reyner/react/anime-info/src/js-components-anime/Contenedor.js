11a2359aeed2aa1a40b4230ac7469dac74c244c2
Rey <Rey86581@gmail.com>
2018-11-09 09:34:46 -0600

    Add routes

--- reyner/react/anime-info/src/js-components-anime/Contenedor.js
+++ reyner/react/anime-info/src/js-components-anime/Contenedor.js
@@ -1,43 +1,17 @@
 import React, {Component} from 'react';
-import { NavBar } from './NavBar';
-import { NavBar2 } from './NavBar2';
-import { ContenidoIndexNode } from './ContenidoIndexNode';
-import { ContenidoIndexAjax } from './ContenidoIndexAjax';
-import { ContenidoInfo } from './ContenidoInfo';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Indice } from './Indice';
+import { Naruto } from './Naruto';
 
 export class Contenedor extends Component{
-
-  constructor(props){
-    super(props)
-    this.state = { info: 'index' }
-    this.ClickMasInfo = this.ClickMasInfo.bind(this);
-    this.BotonAtras = this.BotonAtras.bind(this);
-  }
-
-  ClickMasInfo(){
-    this.setState({ info: 'info' });
-  }
-
-  BotonAtras(){
-    this.setState({ info: 'index' })
-  }
-
   render(){
-    if (this.state.info === 'index') {
       return (
-        <div className="container" data-spy="scroll" data-target="#navbarAnimeInforma" data-offset="0">
-          <NavBar estado={this.state.info}/>
-          <ContenidoIndexAjax onClick={this.ClickMasInfo}/>
-        </div>
+        <Router>
+          <div className="container" data-spy="scroll" data-target="#navbarAnimeInforma" data-offset="0">
+            <Route exact path="/" component={Indice}/>
+            <Route exact path="/naruto" component={Naruto}/>
+          </div>
+        </Router>
       );
-    }
-    if(this.state.info === 'info') {
-      return (
-        <div className="container" data-spy="scroll" data-target="#navbarAnimeInforma" data-offset="0">
-          <NavBar estado={this.state.info}/>
-          <ContenidoInfo onClick={this.BotonAtras}/>
-        </div>
-      );
-    }
   }
 }
