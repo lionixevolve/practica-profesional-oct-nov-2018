cc6a6ed06fff47b0972ebcef045323b6cc61fdfe
Rey <Rey86581@gmail.com>
2018-11-09 17:12:38 -0600

    Update

--- reyner/react/anime-info/src/js-components-anime/ContenidoIndexAjax.js
+++ reyner/react/anime-info/src/js-components-anime/ContenidoIndexAjax.js
@@ -5,61 +5,82 @@ import { Populares } from './Populares';
 import { Lista } from './Lista';
 import $ from 'jquery';
 
+
+
+
 export class ContenidoIndexAjax extends Component {
   constructor(props) {
-   super(props);
-   this.state = {
-     animes: []
-   }
- }
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
 
-  componentWillMount() {
-    fetch('http://localhost/proyectos/reyner/react/anime-info/src/animes.json', {mode: 'no-cors'})
-      .then((response) => {
-        return response.json()
-      })
-      .then((anime) => {
-        this.setState({ animes: anime })
-        console.log(this.state.animes)
-      })
+  componentDidMount() {
+    fetch("http://localhost/proyectos/reyner/react/anime-info/src/animes.json", {mode: 'no-cors'})
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+          console.log(this.state.items);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error: "Error"
+          });
+          console.log(this.state.error);
+        }
+      )
   }
 
   render(){
+    var animes = this.state.items;
     var listaNuevos = [];
     var listaPopulares = [];
+    var cantidad = animes.length;
 
-    this.state.animes.map(anime => {
-      if (anime.etiqueta === "Populares"){
+    for (let i=0; i<cantidad; i++){
+      if (animes.etiqueta === "Populares"){
         listaPopulares.push(
           <Carta
-          src={anime.src}
-          alt={anime.alt}
-          title={anime.title}
-          info={anime.info}
-          masinfo={anime.masinfo}/>
+          src={animes[i].src}
+          alt={animes[i].alt}
+          title={animes[i].title}
+          info={animes[i].info}
+          masinfo={animes[i].masinfo}/>
         );
       } else {
         listaNuevos.push(
           <Carta
-          src={anime.src}
-          alt={anime.alt}
-          title={anime.title}
-          info={anime.info}
-          masinfo={anime.masinfo}/>
+          src={animes[i].src}
+          alt={animes[i].alt}
+          title={animes[i].title}
+          info={animes[i].info}
+          masinfo={animes[i].masinfo}/>
         );
       }
-    })
+    }
+
 
     return (
       <div>
         <Lista />
         <Populares />
         <div className="row">
-
+        {listaPopulares}
         </div>
         <Nuevos/>
         <div className="row">
-
+        {listaNuevos}
         </div>
       </div>
     );
