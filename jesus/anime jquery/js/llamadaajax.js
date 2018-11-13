5d5155a2a50c635a7dba63254766cd4921535728
JesusD <jesusdariel_00@hotmail.com>
2018-11-13 15:49:42 -0600

    Fix Img's

--- jesus/anime jquery/js/llamadaajax.js
+++ jesus/anime jquery/js/llamadaajax.js
@@ -2,7 +2,7 @@
   function MostrarAnimes(){
     $.ajax({
      method: 'GET',
-     url: 'http://192.168.100.58/anime%20jquery/listaDeAnimes.json',
+     url: 'http://192.168.100.62/AutoPag/php/animes.json',
      dataType: "json",
              success: function(respuesta) {
               var datos = respuesta;
@@ -10,14 +10,14 @@
             for(i = 0; i < datos.length; i++){
               if (datos[i].label == "Populares"){
                 $('#Table').append('<tr>'+
-                '<td align="center" style="dislay: none;" >' + '<div>' +
-                '<img class="img-fluid imgAjax" src="'  + datos[i].src + '">' + '</div>' + '<h6>' + datos[i].title + '</h6>' +
+                '<td align="center" style="dislay: none;" >' +
+                '<img class=" imgAjax" src="'  + datos[i].src + '">' + '<h6>' + datos[i].title + '</h6>' +
                  datos[i].description  + '<br><br>' + ' <button type="button" class="btn btn-primary bg-info">Ver más</button>'  + '</td>' + '</tr>' + '<br><br>')
             }
               else {
                 $('#TableEstrenos').append('<tr>'+
-                '<td align="center" style="dislay: none;" >' + '<div>' +
-                '<img class="img-fluid imgAjax" src="'  + datos[i].src + '">' + '</div>' + '<h6>' + datos[i].title + '</h6>' +
+                '<td align="center" style="dislay: none;" >' +
+                '<img class=" imgAjax" src="'  + datos[i].src + '">' + '<h6>' + datos[i].title + '</h6>' +
                  datos[i].description  + '<br><br>' + ' <button type="button" class="btn btn-primary bg-info">Ver más</button>'  + '</td>' + '</tr>' + '<br><br>')
             }
             };
