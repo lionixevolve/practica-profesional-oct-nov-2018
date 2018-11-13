141afcba2b6ad1631a9cd6a75ee61cbbee65571f
JesusD <jesusdariel_00@hotmail.com>
2018-11-13 09:21:08 -0600

    Delete for

--- jesus/anime jquery/js/llamadaajax.js
+++ jesus/anime jquery/js/llamadaajax.js
@@ -7,7 +7,6 @@ function MostrarAnimes(){
               var datos = respuesta;
                $('#Table').append('<tr><td width="500"></td>'+
                 '<td align="center"><h4>Descripción</h4></td>');
-            for(i = 0; i < datos.length; i++){
               $('#Table').append('<tr>'+
               '<td align="center" style="dislay: none;" >' +'<img class="img-fluid" src="'  + datos[0].Animes[0].src + '">'  + '</td>' +
               '<td style="dislay: none;" >' + datos[0].Animes[0].Descripción + '<br><br>' + '<a href="' + datos[0].Animes[0].Link + '"><button class="form-control bg-info text-white">Ver </button> </a>' + '</td>'+'</tr>' +
@@ -20,7 +19,7 @@ function MostrarAnimes(){
 
               '<td align="center" style="dislay: none;" >' + '<div>' +'<img class="img-fluid" src="'  + datos[0].Animes[3].src + '">' + '</div>'  + '</td>'+
               '<td style="dislay: none;" >' + datos[0].Animes[3].Descripción + '<br><br>' + '<a href="' + datos[0].Animes[3].Link + '"><button class="form-control bg-info text-white">Ver </button> </a>' + '</td>'+'</tr>'
-            )};
+            );
             }
           })
   }
@@ -35,7 +34,6 @@ function MostrarEstrenos(){
               var datos = respuesta;
                $('#TableEstrenos').append('<tr><td width="500"></td>'+
                 '<td align="center"><h4>Descripción</h4></td>');
-            for(i = 0; i < datos.length; i++){
               $('#TableEstrenos').append('<tr>'+
               '<td align="center" style="dislay: none;" >' + '<div>' +'<img class="img-fluid" src="'  + datos[0].Estrenos[0].src + '">' + '</div>'  + '</td>'+
               '<td style="dislay: none;" >' + datos[0].Estrenos[0].Descripción + '<br><br>' + '<a href="' + datos[0].Estrenos[0].Link + '"><button class="form-control bg-info text-white">Ver </button> </a>' + '</td>'+'</tr>' +
@@ -45,7 +43,7 @@ function MostrarEstrenos(){
 
               '<td align="center" style="dislay: none;" >' + '<div>' +'<img class="img-fluid" src="'  + datos[0].Estrenos[2].src + '">' + '</div>'  + '</td>'+
               '<td style="dislay: none;" >' + datos[0].Estrenos[2].Descripción + '<br><br>' + '<a href="' + datos[0].Estrenos[2].Link + '"><button class="form-control bg-info text-white">Ver </button> </a>' + '</td>'+'</tr>'
-            )};
+            );
             }
           })
   }
