fce98c7b6bd994b3f56ccc5fdfdb198560735243
JesusD <jesusdariel_00@hotmail.com>
2018-11-13 12:23:57 -0600

    Add jax title,Delete info,read othersjson

--- jesus/anime jquery/js/llamadaajax.js
+++ jesus/anime jquery/js/llamadaajax.js
@@ -2,25 +2,21 @@
   function MostrarAnimes(){
     $.ajax({
      method: 'GET',
-     url: 'http://localhost/anime%20jquery/listaDeAnimes.json',
+     url: 'http://192.168.100.51/proyectos/reyner/react/anime-info/src/animes2.json',
      dataType: "json",
              success: function(respuesta) {
               var datos = respuesta;
 
             for(i = 0; i < datos.length; i++){
-              if (datos[i].label == "Animes"){
-                $('#Table').append('<tr><td width="500"></td>'+
-                 '<td align="center"><h4>Descripción</h4></td>');
+              if (datos[i].label == "Populares"){
                 $('#Table').append('<tr>'+
                 '<td align="center" style="dislay: none;" >' + '<div>' + '<img class="img-fluid" src="'  + datos[i].src + '">' + '</div>'  + '</td>' +
-                '<td style="dislay: none;" >' + datos[i].description + '</td>'+'</tr>')
+                '<td style="dislay: none;" >' + '<h6>' + datos[i].title + '</h6>' + datos[i].description + '</td>' + '</tr>')
             }
               else {
-                $('#TableEstrenos').append('<tr><td width="500"></td>'+
-                 '<td align="center"><h4>Descripción</h4></td>');
                 $('#TableEstrenos').append('<tr>'+
                 '<td align="center" style="dislay: none;" >' + '<div>' + '<img class="img-fluid" src="'  + datos[i].src + '">' + '</div>'  + '</td>' +
-                '<td style="dislay: none;" >' + datos[i].description + '</td>'+'</tr>')
+                '<td style="dislay: none;" >' + '<h6>' + datos[i].title + '</h6>' + datos[i].description + '</td>'+'</tr>')
             }
             };
             }
