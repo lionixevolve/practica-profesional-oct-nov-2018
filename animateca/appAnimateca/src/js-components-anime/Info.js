6c74100e51364ed58fda18a4db7dc22bfee83d29
Rey <Rey86581@gmail.com>
2018-11-15 13:53:23 -0600

    Other changes

--- animateca/appAnimateca/src/js-components-anime/Info.js
+++ animateca/appAnimateca/src/js-components-anime/Info.js
@@ -4,10 +4,10 @@ export const Info = () => {
   return (
     <div className="row">
       <div className="col-md-6 col-sm-12">
-        <img className="img-fluid rounded" alt="Naruto Shippuden" src="../imagenes/A3E.jpg"/>
+        <img className="img-fluid rounded" alt="Naruto Shippuden" src="https://steemitimages.com/DQmUnkJ7Qoub1kejcg4jRA5QoV9cJFAqK15W9VSLPuMxDF5/A3E.jpg"/>
       </div>
       <div className="bg-transparent col-md-6 col-sm-12" id="CartaTexto">
-        <img className="card-img img-fluid" id="ImgFondo" src="../imagenes/tailed_beast_naruto_minimalist_wallpaper_by_zacharychua-d9u3mwd.png" alt="Bg imagen"/>
+        <img className="card-img img-fluid" id="ImgFondo" src="https://i1.wp.com/ramenswag.com/wp-content/uploads/2016/05/tailed_beast_naruto_minimalist_wallpaper_by_zacharychua-d9u3mwd.png?ssl=1" alt="Bg imagen"/>
         <div className="card-img-overlay">
           <h5 className="card-title text-white"><strong>Sinopsis</strong></h5>
           <p className="card-text text-justify text-white">Naruto ha pasado éstos últimos 3 años adiestrando al lado de Jiraiya, el profesor pervertido, mientras que los demás han alcanzado ya rangos superiores, en su mayor parte chunin, naruto en su instante no lo consiguió por el ataque a Konoha a lo largo de los examenes, y después con su ausencia no pudo regresar a presentarse.</p>
--- reyner/react/anime-info/src/js-components-anime/Titulo.js
+++ reyner/react/anime-info/src/js-components-anime/Titulo.js
@@ -10,7 +10,7 @@ export const Titulo = (props) => {
           <h5><Link to="/" id="atras" className="badge badge-dark mt-2 text-white" data-toggle="tooltip" data-placement="top" title="Volver a la pagina anterior"><span className="fi-arrow-left" aria-hidden="true"></span></Link></h5>
         </div>
         <div className="col-10">
-          <h3 className="text-center"><img id="IconoNaruto" alt="Logo konoha" name="Konoha" src="../imagenes/Naruto-konoha-symbol-i7.png"/> Naruto Shippuden</h3>
+          <h3 className="text-center"><img id="IconoNaruto" alt="Logo konoha" name="Konoha" src="https://vignette.wikia.nocookie.net/shinobito/images/9/98/Naruto-konoha-symbol-i7.png/revision/latest?cb=20120602011750"/> Naruto Shippuden</h3>
         </div>
       </div>
       <hr/>
