17ad7e61f3c71ff4d7c165b73f7c6c34fe5cf3bd
JesusD <jesusdariel_00@hotmail.com>
2018-11-16 16:50:21 -0600

    Promise ajax

--- jesus/anime promises/js/llamadaajax.js
+++ jesus/anime promises/js/llamadaajax.js
@@ -4,42 +4,37 @@
 // jesus   http://192.168.100.58/anime%20jquery/listaDeAnimes.json
 
 
+function MostrarAnimesJesus(){
+  $.get('http://192.168.1.8/anime%20jquery/listaDeAnimes.json')
+    .done(function(datos) {
+      for(i = 0; i < datos.length; i++){
+        if (datos[i].label == "Populares"){
+          $('#TableJesus').append('<tr>'+
+          '<td align="center" style="dislay: none;" >' +
+          '<img class=" imgAjax" src="'  + datos[i].src + '">' + '<h6>' + datos[i].title + '</h6>' +
+           datos[i].description  + '<br><br>' + ' <button type="button" class="btn btn-primary bg-info">Ver más</button>'  + '</td>' + '</tr>' + '<br><br>')
+      }
+        else {
+          $('#TableJesusNuevos').append('<tr>'+
+          '<td align="center" style="dislay: none;" >' +
+          '<img class=" imgAjax" src="'  + datos[i].src + '">' + '<h6>' + datos[i].title + '</h6>' +
+           datos[i].description  + '<br><br>' + ' <button type="button" class="btn btn-primary bg-info">Ver más</button>'  + '</td>' + '</tr>' + '<br><br>')
+      }
+      };
+    })
+    .fail(function(error) {
+      toastr["error"]("No se logró cargar", "Error")
+    })
+}
 
-new Promise((resolve,reject) => {
-    $.ajax({
-         method: 'GET',
-         url: 'http://192.168.1.8/anime%20jquery/listaDeAnimes.json',
-         dataType: "json",
-          success: function(datos) {
-                  toastr.success('Cargo Correctamente')
-                  for(i = 0; i < datos.length; i++){
-                    if (datos[i].label == "Populares"){
-                      $('#TableJesus').append('<tr>'+
-                      '<td align="center" style="dislay: none;" >' +
-                      '<img class=" imgAjax" src="'  + datos[i].src + '">' + '<h6>' + datos[i].title + '</h6>' +
-                       datos[i].description  + '<br><br>' + ' <button type="button" class="btn btn-primary bg-info">Ver más</button>'  + '</td>' + '</tr>' + '<br><br>')
-                  }
-                    else {
-                      $('#TableJesusNuevos').append('<tr>'+
-                      '<td align="center" style="dislay: none;" >' +
-                      '<img class=" imgAjax" src="'  + datos[i].src + '">' + '<h6>' + datos[i].title + '</h6>' +
-                       datos[i].description  + '<br><br>' + ' <button type="button" class="btn btn-primary bg-info">Ver más</button>'  + '</td>' + '</tr>' + '<br><br>')
-                  }
-                  };
-                },
-                error: function(){
-                  toastr.error('Error')
-                }
-    });
-  });
+function tiempoIntervalo(){
 
-
-
+}
 
 
 /*    $.ajax({
      method: 'GET',
-     url: 'http://192.168.100.51/anime%20jquery/listaDeAnimes.json',
+     url: 'http://192.168.1.8/anime%20jquery/listaDeAnimes.json',
      dataType: "json",
       success: function(respuesta) {
               var datos = respuesta;
