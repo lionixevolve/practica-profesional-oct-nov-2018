caaa4ee4cb60e69677899129a0cbed48fbba3f77
JesusD <jesusdariel_00@hotmail.com>
2018-11-19 14:46:01 -0600

    Add show nuevos

--- jesus/anime promises/js/llamadaajax.js
+++ jesus/anime promises/js/llamadaajax.js
@@ -57,6 +57,7 @@ function CargarAnimeJaycob() {
 
 
 
+//Agrega los animes
 
 function AgregarAnimeJesus(response){
    var datos=response;
@@ -112,80 +113,3 @@ function AgregarAnimeJaycob(response){
   }
 }
 }
-
-
-/*    $.ajax({
-     method: 'GET',
-     url: 'http://192.168.1.8/anime%20jquery/listaDeAnimes.json',
-     dataType: "json",
-      success: function(respuesta) {
-              var datos = respuesta;
-            for(i = 0; i < datos.length; i++){
-              if (datos[i].label == "Populares"){
-                $('#TableJesus').append('<tr>'+
-                '<td align="center" style="dislay: none;" >' +
-                '<img class=" imgAjax" src="'  + datos[i].src + '">' + '<h6>' + datos[i].title + '</h6>' +
-                 datos[i].description  + '<br><br>' + ' <button type="button" class="btn btn-primary bg-info">Ver más</button>'  + '</td>' + '</tr>' + '<br><br>')
-            }
-              else {
-                $('#TableJesusNuevos').append('<tr>'+
-                '<td align="center" style="dislay: none;" >' +
-                '<img class=" imgAjax" src="'  + datos[i].src + '">' + '<h6>' + datos[i].title + '</h6>' +
-                 datos[i].description  + '<br><br>' + ' <button type="button" class="btn btn-primary bg-info">Ver más</button>'  + '</td>' + '</tr>' + '<br><br>')
-            }
-            };
-            }
-          })
-  }
-
-
-    function MostrarAnimesReyner(){
-      $.ajax({
-       method: 'GET',
-       url: 'http://192.168.100.53/proyectos/reyner/react/anime-info/src/animes2.json',
-       dataType: "json",
-               success: function(respuesta) {
-                var datos = respuesta;
-              for(i = 0; i < datos.length; i++){
-                if (datos[i].label == "Populares"){
-                  $('#TableReyner').append('<tr>'+
-                  '<td align="center" style="dislay: none;" >' +
-                  '<img class=" imgAjax" src="'  + datos[i].src + '">' + '<h6>' + datos[i].title + '</h6>' +
-                   datos[i].description  + '<br><br>' + ' <button type="button" class="btn btn-primary bg-info">Ver más</button>'  + '</td>' + '</tr>' + '<br><br>')
-              }
-                else {
-                  $('#TableReynerNuevos').append('<tr>'+
-                  '<td align="center" style="dislay: none;" >' +
-                  '<img class=" imgAjax" src="'  + datos[i].src + '">' + '<h6>' + datos[i].title + '</h6>' +
-                   datos[i].description  + '<br><br>' + ' <button type="button" class="btn btn-primary bg-info">Ver más</button>'  + '</td>' + '</tr>' + '<br><br>')
-              }
-              };
-              }
-            })
-    }
-
-
-    function MostrarAnimesJaycob(){
-        $.ajax({
-         method: 'GET',
-         url: 'http://192.168.100.54/AutoPag/php/animes.json',
-         dataType: "json",
-                 success: function(respuesta) {
-                  var datos = respuesta;
-                for(i = 0; i < datos.length; i++){
-                  if (datos[i].label == "Populares"){
-                    $('#TableJaycob').append('<tr>'+
-                    '<td align="center" style="dislay: none;" >' +
-                    '<img class=" imgAjax" src="'  + datos[i].src + '">' + '<h6>' + datos[i].title + '</h6>' +
-                     datos[i].description  + '<br><br>' + ' <button type="button" class="btn btn-primary bg-info">Ver más</button>'  + '</td>' + '</tr>' + '<br><br>')
-                }
-                  else {
-                    $('#TableJaycobNuevos').append('<tr>'+
-                    '<td align="center" style="dislay: none;" >' +
-                    '<img class=" imgAjax" src="'  + datos[i].src + '">' + '<h6>' + datos[i].title + '</h6>' +
-                     datos[i].description  + '<br><br>' + ' <button type="button" class="btn btn-primary bg-info">Ver más</button>'  + '</td>' + '</tr>' + '<br><br>')
-                }
-                };
-                }
-              })
-      } */
