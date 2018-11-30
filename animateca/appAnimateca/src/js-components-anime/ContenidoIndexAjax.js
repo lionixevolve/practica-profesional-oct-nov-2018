ee51a96b9d53b1869bbd1c40e18ab63fe4134827
JesusD <jesusdariel_00@hotmail.com>
2018-11-30 16:02:07 -0600

    Fix ajaxUrl

--- animateca/appAnimateca/src/js-components-anime/ContenidoIndexAjax.js
+++ animateca/appAnimateca/src/js-components-anime/ContenidoIndexAjax.js
@@ -20,7 +20,7 @@ export class ContenidoIndexAjax extends Component{
   componentWillMount(){
     $.ajax({
       method: "GET",
-      url: "http://192.168.100.53/miCRM/SuiteCRM/service/v4_1/rest.php?method=get_entries&input_type=JSON&response_type=JSON&rest_data={%20%22session%22:%20%22gt25gvgtpfevfnc4qsctlfmjpm%22,%20%22module_name%22:%20%22Opportunities%22,%20%22ids%22:%20{%20%220%22:%20%221fa006f5-6755-4835-4c0e-5bfda3c92db2%22,%20%221%22:%20%2223a7adbc-b941-8634-1aef-5bfda4678b5b%22,%20%222%22:%20%2246530580-8e2f-093c-cb1f-5bfda118ffc3%22,%20%223%22:%20%2266c24dbd-34af-97b7-482d-5bfda1766021%22,%20%224%22:%20%228f71b9f6-336a-77ee-9e47-5bfda471e39a%22,%20%225%22:%20%22bbe4fab2-f787-108b-20cc-5bfda486c7c7%22,%20%226%22:%20%22bbf21796-ee7c-c7ff-6437-5bfda38757b9%22%20},%20%22select_fields%22:%20{%20%22name%22:%20%22name%22,%20%22description%22:%20%22description%22,%20%22alt_c%22:%20%22alt_c%22,%20%22moreinfo_c%22:%20%22moreinfo_c%22,%20%22src_c%22:%20%22src_c%22,%20%22category_c%22:%20%22category_c%22%20},%20%22link_name_to_fields_array%22:%20{}%20}",
+      url: "http://192.168.100.53/miCRM/SuiteCRM/service/v4_1/rest.php?method=get_entry_list&input_type=JSON&response_type=JSON&rest_data={%20%22session%22:%20%22gt25gvgtpfevfnc4qsctlfmjpm%22,%20%22module_name%22:%20%22Opportunities%22,%20%22query%22:%20%22%22,%20%22order_by%22:%20%22%22,%20%22offset%22:%200,%20%22select_fields%22:%20{%22name%22:%22name%22,%20%22description%22:%22description%22,%22alt_c%22:%22alt_c%22,%20%22moreinfo_c%22:%22moreinfo_c%22,%20%22src_c%22:%22src_c%22,%20%22category_c%22:%22category_c%22},%20%22link_name_to_fields_array%22:%20{},%20%22max_results%22:%2050,%20%22deleted%22:%200%20}",
       dataType: "json",
       success: function(respuesta) {
         this.setState({
